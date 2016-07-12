750790d06824096c6dcf4b0c00edb44664d420a8
Ted Kern <tkern@andrew.cmu.edu>
2016-07-11 19:09:19 -0700

    fixed it for my *nix install

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ EXTRA_DIR="$HOME/.extra"
 
 # Bunch of symlinks
 
-ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/runcom/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/runcom/.gemrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
