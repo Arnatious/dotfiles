850202626e79dd68f31de937ab6b40993c40897e
Ted Kern <tkern@andrew.cmu.edu>
2016-07-14 16:52:17 -0700

    added setup script

---
+++ setupApps.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install vim gitk tmux python3-dev python3-pip
+mkdir /tmp/atom
+wget https://atom.io/download/deb -O /tmp/atom/atom.deb
+sudo dpkg -i /tmp/atom/atom.deb
+sudo rm -rf /tmp/atom
+
+sudo -H pip3 install thefuck
+source install.sh
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+
+echo "TIME FOR REBOOT"
