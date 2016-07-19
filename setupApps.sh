702e15a49211dc89afb66773da8fd80edde444c7
Ted Kern <tkern@andrew.cmu.edu>
2016-07-18 19:44:58 -0700

    atom now configured to install my package set

--- setupApps.sh
+++ setupApps.sh
@@ -5,11 +5,15 @@ mkdir /tmp/atom
 wget https://atom.io/download/deb -O /tmp/atom/atom.deb
 sudo dpkg -i /tmp/atom/atom.deb
 sudo rm -rf /tmp/atom
+apm install `cat atom/packages.list`
 
 sudo -H pip3 install thefuck
 source install.sh
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+
+
+source install.sh
 
 echo "TIME FOR REBOOT"
