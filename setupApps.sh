0a3a1e3bbebf3fab6db59555e6f8096991726c06
Ted Kern <tkern@andrew.cmu.edu>
2017-02-20 19:56:05 -0500

    updated setupApps with screen and no ROS

--- setupApps.sh
+++ setupApps.sh
@@ -1,19 +1,14 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install vim gitk tmux python3-dev python3-pip
+sudo apt-get install screen wget curl  
 mkdir /tmp/atom
 wget https://atom.io/download/deb -O /tmp/atom/atom.deb
 sudo dpkg -i /tmp/atom/atom.deb
 sudo rm -rf /tmp/atom
-apm install `cat atom/packages.list`
+apm install sync-settings
 
 sudo -H pip3 install thefuck
 source install.sh
-
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
-
-
-/bin/bash install.sh
 
 echo "TIME FOR REBOOT"
