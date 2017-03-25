33636cbbf458fe86114403d89fa0b88224b1f375
Ted Kern <tkern@arnatious.com>
2017-03-25 13:51:25 -0400

    added atom ppa, vivaldi setup

--- setupApps.sh
+++ setupApps.sh
@@ -1,14 +1,12 @@
+sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install vim gitk tmux python3-dev python3-pip
-sudo apt-get install screen wget curl  
-mkdir /tmp/atom
-wget https://atom.io/download/deb -O /tmp/atom/atom.deb
-sudo dpkg -i /tmp/atom/atom.deb
-sudo rm -rf /tmp/atom
+sudo apt-get install screen wget curl atom 
+mkdir /tmp/vivaldi
+wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.7.735.46-1_amd64.deb -O /tmp/atom/atom.deb
+sudo dpkg -i /tmp/atom/vivaldi.deb
+sudo rm -rf /tmp/vivaldi
 apm install sync-settings
 
 sudo -H pip3 install thefuck
-source install.sh
-
-echo "TIME FOR REBOOT"
