17f607877e0f894410d0cca4dc5f4289d046407a
Ted Kern <tkern@andrew.cmu.edu>
2017-03-27 10:46:52 -0400

    better ubuntu atom/vivaldi setup

--- setupApps.sh
+++ setupApps.sh
@@ -1,12 +1,13 @@
-sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install vim gitk tmux python3-dev python3-pip
-sudo apt-get install screen wget curl atom 
-mkdir /tmp/vivaldi
-wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.7.735.46-1_amd64.deb -O /tmp/atom/atom.deb
-sudo dpkg -i /tmp/atom/vivaldi.deb
-sudo rm -rf /tmp/vivaldi
+sudo apt-get install -y vim gitk tmux python3-dev python3-pip
+sudo apt-get install -y screen wget curl
+mkdir /tmp/packs
+wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.7.735.46-1_amd64.deb -O /tmp/packs/vivaldi.deb
+wget https://atom.io/download/deb -O /tmp/packs/atom.deb
+sudo dpkg -i /tmp/packs/vivaldi.deb
+sudo dpkg -i /tmp/packs/atom.deb
+sudo rm -rf /tmp/packs
 apm install sync-settings
 
 sudo -H pip3 install thefuck
