4195e1969575a94255c2fd17eb37ec67cfa5f873
Ted Kern <tkern@arnatious.com>
2017-05-22 00:21:56 -0400

    removed keychain from profile

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ ln -sfv "$DOTFILES_DIR/runcom/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/runcom/.gemrc" ~
+ln -sfv "$DOTFILES_DIR/system/.dir_colors" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/vim/vimrc" ~/.vimrc
--- runcom/.bash_profile
+++ runcom/.bash_profile
@@ -1,7 +1,3 @@
-if hash keychain 2>/dev/null; then
-  keychain &> /dev/null
-fi
-
-if [ -f ~/.bashrc ]; then
-   source ~/.bashrc
+if [ -f $HOME/.bashrc ]; then
+   . $HOME/.bashrc
 fi
