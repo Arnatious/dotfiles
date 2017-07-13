b827e3344b0d315a97ab88c4de134039ff0b1e39
Ted Kern <ted.kern@jpl.nasa.gov>
2017-07-13 16:00:14 -0700

    update gitignore and install powerline

--- setupApps.sh
+++ setupApps.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
-set -x
-set -v
 
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y vim gitk tmux python3-dev python3-pip
 sudo apt-get install -y screen wget curl
 mkdir /tmp/packs
+#
+# Get Vivaldi
+#
 if ! dpkg --get-selections | grep -q "^vivaldi-stable[[:space:]]*install$" >/dev/null; then
   wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.7.735.46-1_amd64.deb -O /tmp/packs/vivaldi.deb
   sudo dpkg -i /tmp/packs/vivaldi.deb
 fi
 
+#
+# Get Go
+#
 if [ ! -d /usr/local/go/ ]; then
   wget https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz -O /tmp/packs/go1.8.tar.gz
   tar -C /usr/local -xzf /tmp/packs/go1.8.tar.gz
 fi
 go get -u github.com/odeke-em/drive/cmd/drive
+
+#
+# Get Powerline
+#
+sudo -H pip3 install powerline-status
+git clone --depth=1 -- https://github.com/powerline/fonts.git /tmp/pack/pfonts
+bash /tmp/pack/pfonts/install.sh
+
 sudo rm -rf /tmp/packs
 
 sudo -H pip3 install thefuck
