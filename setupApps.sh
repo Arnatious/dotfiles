0b5f3fda3be65dca02517835703388d890ab5b6e
Ted Kern <tkern@arnatious.com>
2018-06-08 00:27:43 -0700

    minor changes to setup, ros script now an extra

--- setupApps.sh
+++ setupApps.sh
@@ -2,18 +2,8 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y vim gitk tmux python3-dev python3-pip
+sudo apt-get install -y vim gitk tmux python python3 python3-pip
 sudo apt-get install -y screen wget curl
-mkdir /tmp/packs
-
-#
-# Get Go
-#
-if [ ! -d /usr/local/go/ ]; then
-  wget https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz -O /tmp/packs/go1.8.tar.gz
-  tar -C /usr/local -xzf /tmp/packs/go1.8.tar.gz
-fi
-go get -u github.com/odeke-em/drive/cmd/drive
 
 #
 # Get Powerline
@@ -24,4 +14,4 @@ bash /tmp/pack/pfonts/install.sh
 
 sudo rm -rf /tmp/packs
 
-sudo -H pip3 install thefuck
+sudo -H pip install thefuck pipenv
