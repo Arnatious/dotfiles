4a7da5a3336ff8936577142e63405faade73d71d
Ted Kern <tkern@arnatious.com>
2019-04-17 21:16:28 -0700

    fixed python3 reference, updated powerline fonts to use package

--- setupApps.sh
+++ setupApps.sh
@@ -3,15 +3,11 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y vim gitk tmux python python3 python3-pip
-sudo apt-get install -y screen wget curl
+sudo apt-get install -y screen wget curl htop ack fonts-powerline
 
 #
 # Get Powerline
 #
 sudo -H pip3 install powerline-status
-git clone --depth=1 -- https://github.com/powerline/fonts.git /tmp/pack/pfonts
-bash /tmp/pack/pfonts/install.sh
 
-sudo rm -rf /tmp/packs
-
-sudo -H pip install thefuck pipenv
+sudo -H pip3 install thefuck
