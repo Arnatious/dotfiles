bca5d7dcc9cdaf506bb9975d206ba41f3f737e14
Ted Kern <ted.kern@canonical.com>
2020-04-08 02:06:56 -0700

    add xsel to setupapps

--- setupApps.sh
+++ setupApps.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y vim gitk tmux fasd python3 python3-pip
-sudo apt-get install -y screen wget curl htop ack fonts-powerline
+sudo apt-get install -y screen wget curl htop ack fonts-powerline xsel
 
 #
 # Get Powerline
