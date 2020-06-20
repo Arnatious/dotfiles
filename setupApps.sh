772f117c30577be9ad60dbc9749bc71af59aa8ab
Ted Kern <ted.kern@canonical.com>
2020-06-19 22:27:12 -0700

    add dotglob and extglob

--- setupApps.sh
+++ setupApps.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y vim gitk tmux fasd python3 python3-pip
+sudo apt-get install -y vim git gitk gitg tmux python3 python3-pip
 sudo apt-get install -y screen wget curl htop ack fonts-powerline xsel
 
 #
